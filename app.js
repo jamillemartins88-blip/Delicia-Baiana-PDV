b059e868740d8ead65f2ade7b6f9a1908b32c81d
jamillemartins88-blip <jamillemartins88@gmail.com>
2026-03-10 11:43:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1137,7 +1137,7 @@ function init() {
   startAutoBackup();
 
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("service-worker.js")
+    navigator.serviceWorker.register("./service-worker.js")
       .then(() => console.log("Service Worker registrado"))
       .catch((err) => console.log("Erro:", err));
   }
