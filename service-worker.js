3e87cbf86b8756bfb26f7e40ac90e293233658b8
jamillemartins88-blip <jamillemartins88@gmail.com>
2026-03-10 15:23:48 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,51 @@
-self.addEventListener("install", function(event) {
-  console.log("App instalado");
+const CACHE = "delicias-pdv-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => 
+      Promise.all(
+        keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
 
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      
+      return fetch(event.request)
+        .then((response) => {
+          if (!response || response.status !== 200 || response.type !== "basic") {
+            return response;
+          }
+          const copy = response.clone();
+          
+          caches.open(CACHE).then((cache) => {
+            cache.put(event.request, copy);
+        });
+          
+          return response;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
 });
