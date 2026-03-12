deff7e01072f8492366098cf0e21e21aab27d12d
jamillemartins88-blip <jamillemartins88@gmail.com>
2026-03-12 16:11:14 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,6 @@
-const CACHE = "delicia-pdv-cache-v1";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener("install", function(event) {
+  console.log("App instalado");
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) return cached;
-      return fetch(event.request)
-        .then((response) => {
-          if (!response || response.status !== 200 || response.type !== "basic") return response;
-          const copy = response.clone();
-          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match("./index.html"));
-    })
-  );
+self.addEventListener("fetch", function(event) {
 });
