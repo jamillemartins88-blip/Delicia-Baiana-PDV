c04db809589b2b437c27f8b2fddf950d5cd1af35
jamillemartins88-blip <jamillemartins88@gmail.com>
2026-03-12 08:43:17 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,60 +1,28 @@
-const CACHE = "delicias-pdv-cache-v2";
+const CACHE = "pdv-cache-v1";
 
-const ASSETS = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/app.js",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => {
-      return cache.addAll(ASSETS);
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./styles.css",
+        "./app.js",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
     })
   );
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE)
-          .map((key) => caches.delete(key))
-      )
-    )
-  );
-
-  return self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
 
   if (!event.request.url.startsWith("http")) return;
 
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-
-      if (cached) return cached;
-
-      return fetch(event.request).then((response) => {
-
-        const clone = response.clone();
-
-        caches.open(CACHE).then((cache) => {
-          cache.put(event.request, clone);
-        });
-
-        return response;
-
-      }).catch(() => caches.match("/index.html"));
-
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
+
 });
